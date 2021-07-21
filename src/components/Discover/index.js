a10ecb62178b9f6d43c3766a7f3449ce34d3610c
sid6145 <sidtdesh@gmail.com>
2021-07-21 13:34:38 +0530

    added an API in discover component

--- src/components/Discover/index.js
+++ src/components/Discover/index.js
@@ -3,34 +3,23 @@ import axios from 'axios'
 import './style.css'
 
 function Discover() {
-    const [list, setList] = useState([])
+    const [aniList, setAniList] = useState([])
 
-    
-    useEffect(()=> {
-        var options = {
-            method: 'GET',
-            url: 'https://hummingbirdv1.p.rapidapi.com/anime/steins-gate',
-            headers: {
-              'x-rapidapi-key': '011970864emshdb968718f081befp19f279jsn8fdb609d3051',
-              'x-rapidapi-host': 'hummingbirdv1.p.rapidapi.com'
-            }
-          };
-          
-          axios.request(options).then(function (response) {
-              setList(response.data)
-              console.log(response.data);
-          }).catch(function (error) {
-              console.error(error);
-          });
+    useEffect( async () => {
+      let anime = await axios.get('https://kitsu.io/api/edge/anime')
+      setAniList(anime.data.data)
     },[])
 
-    console.log(list)
+  console.log(aniList)
+
 
     return (
         <div className="discover-container">
             <video autoPlay muted loop id="background-video">
                 <source src="discovery-background.mp4" type="video/mp4" />
              </video>
+
+           
         </div>
     )
 }
